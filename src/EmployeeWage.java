149fc278124b933a974ae529635db766a20e2100
Gali-Madhan-Kumar <gmadhan99@gmail.com>
2020-09-08 03:23:25 +0530

    added code to check employee is full time worker or part time worker

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,8 +1,12 @@
 public class EmployeeWage {
 
     private final double empIsPresent = Math.floor(Math.random() * 10) % 2;
+    private final double empIsFullTimeOrPartTime = Math.floor(Math.random() * 10 + 1) % 3;
     private final int wagePerHr = 20;
     private final int noOfHrsPerDay = 8;
+    private final int empIsFullTime = 1;
+    private final int empIsPartTime = 2;
+    private final int empPartTimeHrs = 8;
 
     public double getEmpIsPresent() {
         return empIsPresent;
@@ -14,6 +18,22 @@ public class EmployeeWage {
 
     public int getNoOfHrsPerDay() {
         return noOfHrsPerDay;
+    }
+
+    public int getEmpIsFullTime() {
+        return empIsFullTime;
+    }
+
+    public int getEmpIsPartTime() {
+        return empIsPartTime;
+    }
+
+    public int getEmpPartTimeHrs() {
+        return empPartTimeHrs;
+    }
+
+    public double getEmpIsFullTimeOrPartTime() {
+        return empIsFullTimeOrPartTime;
     }
 
     public int getDailyWage() {
--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,18 @@ public class Main {
     public static void main(String[] args) {
         EmployeeWage employeeWage = new EmployeeWage();
         int empIsPresent = (int) employeeWage.getEmpIsPresent();
+        int empIsFullTimeOrPartTime = (int) employeeWage.getEmpIsFullTimeOrPartTime();
         int empWagePerDay = employeeWage.getDailyWage();
         if (empIsPresent == 0) {
             System.out.println("Employee is Present");
         } else {
             System.out.println("Employee is Absent");
         }
-        System.out.println("Employee Daily Wage is : " + empWagePerDay);
+        System.out.println("Employee Wage Per Day is : " + empWagePerDay);
+        if (empIsFullTimeOrPartTime == 1) {
+            System.out.println("Employee is Full Time");
+        } else {
+            System.out.println("Employee is Part Time");
+        }
     }
 }
