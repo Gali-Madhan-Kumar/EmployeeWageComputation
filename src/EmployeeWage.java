281685612f47d408a7c313710c36aea483a63d9c
Gali-Madhan-Kumar <gmadhan99@gmail.com>
2020-09-08 03:03:03 +0530

    added code to calculate employee wage per day

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,8 +1,22 @@
 public class EmployeeWage {
 
     private final double empIsPresent = Math.floor(Math.random() * 10) % 2;
+    private final int wagePerHr = 20;
+    private final int noOfHrsPerDay = 8;
 
     public double getEmpIsPresent() {
         return empIsPresent;
+    }
+
+    public int getWagePerHr() {
+        return wagePerHr;
+    }
+
+    public int getNoOfHrsPerDay() {
+        return noOfHrsPerDay;
+    }
+
+    public int getDailyWage() {
+        return getWagePerHr() * getNoOfHrsPerDay();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,12 @@ public class Main {
     public static void main(String[] args) {
         EmployeeWage employeeWage = new EmployeeWage();
         int empIsPresent = (int) employeeWage.getEmpIsPresent();
+        int empWagePerDay = employeeWage.getDailyWage();
         if (empIsPresent == 0) {
             System.out.println("Employee is Present");
         } else {
             System.out.println("Employee is Absent");
         }
+        System.out.println("Employee Daily Wage is : " + empWagePerDay);
     }
 }
